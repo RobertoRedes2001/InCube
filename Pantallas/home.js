9b56337106475410020bf52ae36cfb9e2f573f06
JAIME CALVO AREVALO <jacaar@floridauniversitaria.es>
2023-01-31 18:36:39 +0100

    Actualizacion pantalla Home.js

--- Pantallas/home.js
+++ Pantallas/home.js
@@ -1,66 +1,151 @@
 import * as React from 'react';
-import { TextInput, Button } from 'react-native-paper';
-import { StyleSheet, View } from 'react-native';
+import { TextInput, Button, Text } from 'react-native-paper';
+import Constants from 'expo-constants';
+import Icon from 'react-native-ionicons';
+import { useState, useEffect } from 'react';
+import { IconButton } from 'react-native-paper';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
+import { Dimensions } from 'react-native';
 
-export default function Home(props) {
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+
+
+const home = (props) => {
+
+  const [user, setUser] = useState("Paco");
+
+const data = {
+  labels: ['15:00', '16:00', '17:00', '18:00', '19:00', '20:00'],
+  datasets: [
+    {
+      data: [400, 425, 500, 550, 450, 470],
+    },
+  ],
+};
+
+
   return (
-    <View style={styles.layout}>
-    <View flexDirection='row'>
-      <Button
-        style={styles.width}
+    <SafeAreaView style={styles.layout}>
+      <View style={styles.top}>
+        <View style={styles.izquierda}>
+          <Text style={styles.textoUser}> Hola {user}, Bienvenido</Text>
+        </View>
+        <View style={styles.derecha}>
+          <IconButton
+            style={styles.buttonBack}
+            alignSelf="center"
+            icon="backburger"
+            color="white"
+            size={35}
+          />
+        </View>
+      </View>
+      <BarChart
+        data={data}
+        width={screenWidth}
+        height={200}
+        
+        chartConfig={{
+          backgroundColor: 'white',
+          backgroundGradientFrom: '#3c525b',
+          backgroundGradientTo: '#3c525b', // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          decimalPlaces: 0,
+        }}
+        style={{
+          marginRight: 24,
+        }}
+      />
+
+      <LineChart
+        data={data}
+        width={Dimensions.get('window').width} // from react-native
+        height={350}
+        yAxisLabel=""
+        yAxisSuffix=" A"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: 'white',
+          backgroundGradientFrom: '#3c525b',
+          backgroundGradientTo: '#3c525b',
+          decimalPlaces: 0, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+          propsForDots: {
+            r: '6',
+            strokeWidth: '2',
+            stroke: 'orange',
+          },
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+          marginRight: 20,
+        }}
+      />
+      <IconButton
+        style={styles.button}
         alignSelf="center"
-        mode='contained'
-        color='#f0ead6'
-        textColor="black"
-        onPress={() => props.navigation.navigate('screenChart')}>
-        Entrar
-      </Button>
-      <Button
-        style={styles.width}
-        alignSelf="center"
-        mode='contained'
-        color='#f0ead6'
-        textColor="black"
-        onPress={() => props.navigation.navigate('screenChart')}>
-        Entrar
-      </Button>
-    </View>
-    <View flexDirection='row'>
-      <Button
-        style={styles.width}
-        alignSelf="center"
-        mode='contained'
-        color='#f0ead6'
-        textColor="black"
-        onPress={() => props.navigation.navigate('screenChart')}>
-        Entrar
-      </Button>
-      <Button
-        style={styles.width}
-        alignSelf="center"
-        mode='contained'
-        color='#f0ead6'
-        textColor="black"
-        onPress={() => props.navigation.navigate('screenChart')}>
-        Entrar
-      </Button>
-    </View>
-    </View>
+        icon="format-list-bulleted"
+        color="white"
+        size={40}
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   layout: {
     flex: 1,
-    justifyContent: 'center',
-    padding: 8,
-    backgroundColor: 'orange',
-    alignItems: 'center'
+    backgroundColor: '#3c525b',
+    alignItems: 'center',
   },
-  width: {
-    marginLeft: 24,
-    marginRight: 24,
-    fontSize: 18,
-    marginBottom: 24,
+  top: {
+    flexDirection: 'row',
+    backgroundColor: 'orange',
+    height: screenHeight / 10,
+    width: screenWidth,
+    marginBottom: 50,
+  },
+  izquierda: {
+    flex: 1,
+    justifyContent:"flex-end",
+  },
+  derecha: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  textoUser: {
+    color:"white",
+    fontWeight:"bold",
+    marginBottom:10,
+    marginLeft:10,
+  },
+
+  button: {
+    borderWidth: 3,
+    borderColor: 'white',
+    backgroundColor: 'orange',
+  },
+  buttonBack: { 
+    marginBottom:-4,
+    marginRight:-1,
   },
 });
+
+export default home;
