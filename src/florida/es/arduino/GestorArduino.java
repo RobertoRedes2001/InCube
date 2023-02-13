67f200125b7cb0243aec29bb556527ac42b140c2
edinsky12 <91280755+edinsky12@users.noreply.github.com>
2023-02-13 14:57:00 +0100

    Update GestorArduino.java

--- src/florida/es/arduino/GestorArduino.java
+++ src/florida/es/arduino/GestorArduino.java
@@ -22,7 +22,7 @@ public class GestorArduino {
 		try {
 			output.print(String.valueOf(codigoInstruccion));
 			output.flush();
-			if (codigoInstruccion >= 5) {  //Si el codigo es igual o mayor que 5 hay lectura desde el Arduino a Java
+			if (codigoInstruccion >= 9) {  //Si el codigo es igual o mayor que 5 hay lectura desde el Arduino a Java
 				respuesta = input.nextLine();
 			}
 		} catch (Exception e) {
