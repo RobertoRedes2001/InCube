cdca692574f0f34acc80b47aad17091398070bba
edinsky12 <91280755+edinsky12@users.noreply.github.com>
2023-02-14 11:35:16 +0100

    Update Servidor.java

--- src/florida/es/arduino/Servidor.java
+++ src/florida/es/arduino/Servidor.java
@@ -3,7 +3,13 @@ package florida.es.arduino;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -13,57 +19,54 @@ import com.sun.net.httpserver.HttpServer;
 
 public class Servidor {
 
-    public static void main(String[] args) throws Exception {
-    	
-    	System.out.println("Arranca el servidor");
-    	
-    	//String rutaFicheroConfiguracion = args[0];  //Pasar como parametro la ruta del fichero de configuracion
-    	String rutaFicheroConfiguracion = "config.txt";
-    	File ficheroConfiguracion = new File(rutaFicheroConfiguracion);
-    	FileReader fr = new FileReader(ficheroConfiguracion);
-    	BufferedReader br = new BufferedReader(fr);
-    	String host = br.readLine().split("=")[1];
-    	System.out.println("IP: " + host);
-    	int puerto = Integer.parseInt(br.readLine().split("=")[1]);
-    	System.out.println("Puerto TCP: " + puerto);
-    	String rutaRespuesta = br.readLine().split("=")[1];   
-    	System.out.println("Ruta contexto: " + rutaRespuesta);
-    	
-    	InetSocketAddress direccionTCPIP = new InetSocketAddress(host,puerto);
-    	int backlog = Integer.parseInt(br.readLine().split("=")[1]); //Numero de conexiones pendientes que el servidor puede mantener en cola
-    	HttpServer servidor = HttpServer.create(direccionTCPIP, backlog);
-    	
-    	SerialPort[] portNames = SerialPort.getCommPorts();
-    	System.out.println("Puertos serie disponibles:");
-		for (int i = 0; i < portNames.length; i++) {
-			System.out.println((i+1) + ": " + portNames[i].getSystemPortName());
-		}
-		if (portNames.length == 0) {
-			System.out.println("No se han encontrado puertos");
-			return;
-		}
-		Scanner teclado = new Scanner(System.in);
-		System.out.print("Seleccionar puerto serie: ");
-		int numeroPuerto = Integer.parseInt(teclado.nextLine());
-		SerialPort chosenPort = SerialPort.getCommPort(portNames[numeroPuerto-1].getSystemPortName());
-		chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
-		if (chosenPort.openPort()) {
-			System.out.println("Puerto serie abierto");
-		} else {
-			System.out.println("Error abriendo puerto serie");
-		}
-    	
-    	GestorHTTP gestorHTTP = new GestorHTTP(chosenPort);   //Clase que gestionara los GETs, POSTs, etc.
-    	servidor.createContext(rutaRespuesta, gestorHTTP);   //Crea un contexto, asocia la ruta al gestor HTTP
-    	
-    	int numThreads = Integer.parseInt(br.readLine().split("=")[1]);
-    	ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor)Executors.newFixedThreadPool(numThreads);
-    	servidor.setExecutor(threadPoolExecutor); 
-    	
-    	servidor.start();
-    	System.out.println("Servidor HTTP arranca en el puerto TCP " + puerto);
-    	br.close();
-    	
-    }
+	static ServerSocket socketEscucha = null;
+	static Socket cliente = null;
 
+	public static void main(String[] args) throws IOException {
+
+		try {
+
+			try {
+				socketEscucha = new ServerSocket(5001);
+				cliente = socketEscucha.accept();
+				System.out.println("Cliente arduino conectado");
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			String rutaFicheroConfiguracion = "config.txt";
+			File ficheroConfiguracion = new File(rutaFicheroConfiguracion);
+			FileReader fr = new FileReader(ficheroConfiguracion);
+			BufferedReader br = new BufferedReader(fr);
+			
+			//LECTURA CONFIG
+			
+			String host = br.readLine().split("=")[1];
+			int puerto = Integer.parseInt(br.readLine().split("=")[1]);
+			String contexto = br.readLine().split("=")[1];
+			int backlog = Integer.parseInt(br.readLine().split("=")[1]);
+			int numHilos = Integer.parseInt(br.readLine().split("=")[1]);
+			int intervalo = Integer.parseInt(br.readLine().split("=")[1]);
+			
+			System.out.println("IP: " + host);
+			System.out.println("Puerto TCP: " + puerto);
+			InetSocketAddress direccionTCPIP = new InetSocketAddress(host, puerto);
+			HttpServer servidor = HttpServer.create(direccionTCPIP, backlog);
+			
+			
+			GestorHTTP gestorHTTP = new GestorHTTP(cliente);
+			servidor.createContext(contexto, gestorHTTP);
+			
+			ThreadPoolExecutor threadPoolExecutor = (ThreadPoolExecutor) Executors.newFixedThreadPool(numHilos);
+			servidor.setExecutor(threadPoolExecutor);
+			servidor.start();
+			System.out.println("Servidor HTTP arranca en el puerto " + puerto);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
